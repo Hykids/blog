ce8a12274bb0cfb934388e97f9559555d3b88820
chenyian <2587439923@qq.com>
2023-02-16 13:38:44 +0800

    init

---
+++ assets/get_start.9ecd9826.js
@@ -0,0 +1,20 @@
+import{jsx as e,jsxs as r,Fragment as a}from"react/jsx-runtime";const h=void 0,s=[{id:"getting-started",text:"Getting Started",depth:2},{id:"\u9879\u76EE\u4ECB\u7ECD",text:"\u9879\u76EE\u4ECB\u7ECD",depth:3}];function i(n){const t=Object.assign({h2:"h2",a:"a",hr:"hr",p:"p",h3:"h3"},n.components);return r(a,{children:[r(t.h2,{id:"getting-started",children:[e(t.a,{className:"header-anchor","aria-hidden":"true",href:"#getting-started",children:"#"}),"Getting Started"]}),`
+`,e(t.hr,{}),`
+`,e(t.p,{children:"\u81EA\u6211\u4ECB\u7ECD\uFF0C"}),`
+`,e(t.p,{children:e(t.a,{href:"https://visiky.github.io/resume/?mode=edit&template=template3&user=Hykids",target:"_blank",rel:"nofollow",children:"\u8BE6\u8BF7\u6233\u6211"})}),`
+`,r(t.h3,{id:"\u9879\u76EE\u4ECB\u7ECD",children:[e(t.a,{className:"header-anchor","aria-hidden":"true",href:"#\u9879\u76EE\u4ECB\u7ECD",children:"#"}),"\u9879\u76EE\u4ECB\u7ECD"]}),`
+`,e(t.hr,{}),`
+`,e(t.p,{children:"\u5F85\u5B8C\u5584"})]})}function c(n={}){const{wrapper:t}=n.components||{};return t?e(t,Object.assign({},n,{children:e(i,n)})):i(n)}const o="Invalid Date",l=`## Getting Started\r
+\r
+---\r
+\r
+\u81EA\u6211\u4ECB\u7ECD\uFF0C\r
+\r
+[\u8BE6\u8BF7\u6233\u6211](https://visiky.github.io/resume/?mode=edit&template=template3&user=Hykids)\r
+\r
+### \u9879\u76EE\u4ECB\u7ECD\r
+\r
+---\r
+\r
+\u5F85\u5B8C\u5584\r
+`;export{l as content,c as default,h as frontmatter,o as lastUpdatedTime,s as toc};
